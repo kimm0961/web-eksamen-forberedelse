7192a33f88a6fa728077b8af5b3e7a994e9e7367
kimmiesjensen <kimm0961@videndjurs.net>
2020-09-28 09:39:04 +0200

    clean

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// bootstrap css (eller scss)
+import 'bootstrap/dist/css/bootstrap.css';
+// eller den minimerede version:
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// bootstrap js
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
